174ce5ab79ac5bb1f70418e0fa66c0bfa5a8f31f
Neeraj Singh <neerajsingh5097@gmail.com>
2026-02-19 09:09:33 +0530

    update

--- src/main/java/com/keshav/main/Main.java
+++ src/main/java/com/keshav/main/Main.java
@@ -3,31 +3,40 @@ package com.keshav.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 import com.keshav.configuration.HibernateConfig;
+import com.keshav.entity.Address;
 import com.keshav.entity.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Address add1 = new Address();
+		add1.setCity("Noida");
+		add1.setState("UP");
 
-		Employee e = new  Employee( "Kunal", "male", 22200);
-		
-		
-		
-		
+		Employee e = new Employee();
+		e.setName("Vinay");
+		e.setGender("Male");
+		e.setSalary(444444);
+		e.setAddress(add1);
+//		add1.setEmployee(e);
+
 		SessionFactory sf = HibernateConfig.getSessionFactory();
+
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
-		
-		session.persist(e);
-		tx.commit();
+//		session.persist(e);
+//		session.persist(add1);
+//		tx.commit();
+
+		Employee employee = session.find(Employee.class,1);
+		System.out.println(employee);
+
+////		Address ad = new Address();
+//		Address address = session.find(Address.class, 15);
+//		System.out.println(address);
+
 	}
 
 }
